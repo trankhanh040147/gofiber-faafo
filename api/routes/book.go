cfd02306c452fbe7e083ce0404e22f302b9eeadb
root <root@tpk2247>
2024-04-14 12:47:01 +0700

    base Clean Architecture with CRUD Books

---
+++ api/routes/book.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gofiber-faafo/api/handlers"
+	"gofiber-faafo/pkg/book"
+)
+
+// BookRouter is the Router for GoFiber App
+func BookRouter(app fiber.Router, service book.Service) {
+	app.Get("/books", handlers.GetBooks(service))
+	app.Post("/books", handlers.AddBook(service))
+	app.Put("/books", handlers.UpdateBook(service))
+	app.Delete("/books", handlers.RemoveBook(service))
+}
